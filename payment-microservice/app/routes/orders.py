6ae54cf16f7b7946a15f3975a231c0fce4dbb8c4
juniorT34 <jj1982268@gmail.com>
2025-04-14 11:32:19 +0200

    Refactor order retrieval to validate 'id' in request body and update API endpoint

--- payment-microservice/app/routes/orders.py
+++ payment-microservice/app/routes/orders.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from starlette.requests import Request
+from fastapi import APIRouter,Request,HTTPException
+#from starlette.requests import Request
 from app.models.order import Order
 import httpx
 import asyncio
@@ -14,10 +14,13 @@ async def get_orders(request: Request):
     body = await request.json()
 
     #validate that 'id' exists in the incoming body
-
+    if "id" not in body:
+        raise HTTPException(status_code=400, detail="Missing 'id' in request body")
+    
+    product_id =  body["id"]
     async with httpx.AsyncClient() as client:
         # Forward the request body as JSON to the external API
-        response = await client.post(f'http://127.0.0.1:8000/products/', json=body)
+        response = await client.post(f'http://127.0.0.1:8000/products/{product_id}', json=body)
         
         return {
             "status_code": response.status_code,
